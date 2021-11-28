6654c48084c0cca220778aa54adf93d0167b6682
Chinh DT <chinhdtth2009021@fpt.edu.vn>
2021-11-28 21:44:36 +0700

    ASM

---
+++ ASM_DUONGTHICHINH/Entities/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM_DUONGTHICHINH.Entities
+{
+    class Register
+    {
+        public string firstName { get; set; }
+        public string lastNmae { get; set; }
+        public string password { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string avatar { get; set; }
+        public int gender { get; set; }
+        public string email { get; set; }
+        public DateTime birthday { get; set; }
+    }
+}
